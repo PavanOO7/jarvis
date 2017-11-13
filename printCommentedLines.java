e88170b38c274e6a262070aa87e6f8f836d73f55
PavanOO7 <31538746+PavanOO7@users.noreply.github.com>
2017-11-13 10:45:42 +0530

    Add files via upload

---
+++ printCommentedLines.java
@@ -0,0 +1,14 @@
+package number;
+
+public class printCommentedLines {
+
+	public static void main(String[] args)
+	{
+
+
+		// the line below this gives an output
+		// \u000d System.out.println("comment executed");
+
+	}
+
+}
